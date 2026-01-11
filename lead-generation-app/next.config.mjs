1394fcfe563efaf30a09a31122919c99c392a771
zrokhit <zrokhit@uwaterloo.ca>
2026-01-11 14:10:34 -0500

    Prevented nextjs from bundling macOS specific library binaries

--- lead-generation-app/next.config.mjs
+++ lead-generation-app/next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  serverExternalPackages: ["@huggingface/transformers", "onnxruntime-node"],
+};
 
-export default nextConfig
+export default nextConfig;
